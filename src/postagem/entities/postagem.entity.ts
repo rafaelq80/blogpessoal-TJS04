2f3681b86f1daf91956ea5c129b64c489e2b2a21
Rafael Queiróz <rafaelproinfo@gmail.com>
2024-04-15 16:04:15 -0300

    Update

--- src/postagem/entities/postagem.entity.ts
+++ src/postagem/entities/postagem.entity.ts
@@ -1,7 +1,7 @@
 ﻿import { IsNotEmpty } from "class-validator";
 import { Column, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
 
-@Entity({name: "tb_postagem"})
+@Entity({name: "tb_postagens"})
 export class Postagem{
 
     @PrimaryGeneratedColumn() // Chave Primária e Auto_Increment
