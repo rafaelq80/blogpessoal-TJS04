35c1301e58c99c3d25814517ad36045c4eea2602
Rafael Queiróz <rafaelproinfo@gmail.com>
2024-04-24 16:29:26 -0300

    Update

--- src/auth/strategy/local.strategy.ts
+++ src/auth/strategy/local.strategy.ts
@@ -1,4 +1,4 @@
-﻿import { Injectable, UnauthorizedException } from '@nestjs/common';
+﻿import { HttpException, HttpStatus, Injectable, UnauthorizedException } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { Strategy } from 'passport-local';
 import { AuthService } from '../services/auth.service';
@@ -13,10 +13,13 @@ export class LocalStrategy extends PassportStrategy(Strategy) {
   }
 
   async validate(username: string, password: string): Promise<any> {
+
     const user = await this.authService.validateUser(username, password);
+
     if (!user) {
-      throw new UnauthorizedException();
+      throw new HttpException('Acesso não autorizado!', HttpStatus.UNAUTHORIZED)
     }
+    
     return user;
   }
 }
